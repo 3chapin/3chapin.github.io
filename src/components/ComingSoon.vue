0790fe4ff35581876b6145d2ae42317d76e2cff6
Jonathan Lowell <jonathan@saasaparilla.com>
2020-05-11 14:56:49 -0600

    Updated page

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="coming">
+    <div class="images">
+      <img src="@/assets/name-plus-cycle.svg" style="width: 100%">
+    </div>
+    <div class="message">
+      $ system reboot in progress...<br>$ system will be available in {{ counter.days }} days {{ counter.hours }} hours {{ counter.minutes }} minutes {{ counter.seconds }} seconds
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComingSoon',
+  data () {
+    return {
+      counter: {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      }
+    }
+  },
+  methods: {
+    countdown () {
+      let d = new Date()
+      let releaseDate = new Date('2020-06-15')
+      releaseDate.setHours(24)
+      let diff = releaseDate - d
+      let days = diff / 86400000
+      this.counter.days = parseInt(days)
+      let hours = (days - parseInt(days)) * 24
+      this.counter.hours = parseInt(hours)
+      let minutes = (hours - parseInt(hours)) * 60
+      this.counter.minutes = parseInt(minutes)
+      let seconds = (minutes - parseInt(minutes)) * 60
+      this.counter.seconds = parseInt(seconds)
+    }
+  },
+  mounted () {
+    window.setInterval (this.countdown, 1000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.coming {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+}
+.images {
+  width: calc(100% - 70px);
+  margin-left: 70px;
+}
+.message {
+  font-family: monospace;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  margin: 150px 0 0 70px;
+  text-align: left;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
-    component: Home
+    component: () => import('@/components/Home.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/coming',
+    name: 'ComingSoon',
+    component: () => import('@/components/ComingSoon.vue')
+  },
+  {
+    path: '*',
+    redirect: '/coming'
   }
 ]
 
