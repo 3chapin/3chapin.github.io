29aac8dac0da9020b0d676187c2c62da69310334
Jonathan Lowell <jonathan@saasaparilla.com>
2020-06-08 14:18:12 -0600

    Changed manifest.json to vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'MJLowell',
+    themeColor: '#131313',
+    msTileColor: '#131313',
+    manifestOptions: {
+      background_color: '#131313'
+    }
+  }
+}
