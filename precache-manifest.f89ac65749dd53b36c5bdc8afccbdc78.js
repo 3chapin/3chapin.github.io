0093f74b60980eee006e986f796db220ba9667f3
3chapin <46701179+3chapin@users.noreply.github.com>
2020-06-22 12:01:04 -0600

    deploy

---
+++ precache-manifest.f89ac65749dd53b36c5bdc8afccbdc78.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dde2aeda09ea99451c0",
+    "url": "/css/app.c91beb5b.css"
+  },
+  {
+    "revision": "3364ebd8f413c47a110b",
+    "url": "/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "b1d21baf164f72c0babab849a3478883",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "a7b6822d3b019a39418490d5c45c951b",
+    "url": "/img/home-bg-2.a7b6822d.png"
+  },
+  {
+    "revision": "e417ef8f3d9216db07372d43f744c04b",
+    "url": "/img/home-header-mobile.e417ef8f.png"
+  },
+  {
+    "revision": "c03c2d18c3cff497edbc6a0ff846ead5",
+    "url": "/img/home-header.c03c2d18.png"
+  },
+  {
+    "revision": "449356bc42ab6be84782ecd9858ee350",
+    "url": "/img/msny-assignments-bg-2.449356bc.png"
+  },
+  {
+    "revision": "8b856bc255fc25d668dd17428d2f1c41",
+    "url": "/img/msny-assignments-example.8b856bc2.png"
+  },
+  {
+    "revision": "ff78049f71a3b6f09498f62e69209da9",
+    "url": "/img/profile.ff78049f.png"
+  },
+  {
+    "revision": "e7954b2f0d377e86debab1442c3a00c1",
+    "url": "/img/svc-assignments-bg-2.e7954b2f.png"
+  },
+  {
+    "revision": "51f60ab5724bb7244d6bb4a2a9a1a8f3",
+    "url": "/img/svc-assignments-example.51f60ab5.png"
+  },
+  {
+    "revision": "3982cd385b31c6e32ecab66d82501603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dde2aeda09ea99451c0",
+    "url": "/js/app.22169d20.js"
+  },
+  {
+    "revision": "3364ebd8f413c47a110b",
+    "url": "/js/chunk-vendors.25235bc5.js"
+  },
+  {
+    "revision": "8daa0191cb67e9fc99fd91fb7aedaff8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
